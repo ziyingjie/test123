12010e6d23d602a0a739ec70d8467367d9eee254
ziying <sivir.liu@intramirror.com>
2022-03-02 10:43:54 +0800

    [✨新增](Vue): 增加222

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About </router-link>
+      <h1>2222</h1>
     </div>
     <router-view />
   </div>
