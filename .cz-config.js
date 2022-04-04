a6b85cd3b14075517d105acf7eae8147845299f6
ziying <sivir.liu@intramirror.com>
2022-04-04 22:04:53 +0800

    feat: aaa

--- .cz-config.js
+++ .cz-config.js
@@ -12,10 +12,11 @@ module.exports = {
     { value: '⏪回滚', name: '回滚:    代码回退' },
   ],
   scopes: [
-    { name: 'Vue' },
-    { name: 'node' },
+    { name: 'leetcode' },
     { name: 'javascript' },
     { name: 'typescript' },
+    { name: 'Vue' },
+    { name: 'node' },
   ],
   // it needs to match the value for field type. Eg.: 'fix'
   /*  scopeOverrides: {
